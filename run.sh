b1c9c57f2105022ed2e5d8bccac3ec7ea4542dcf
Adam Shannon <adamkshannon@gmail.com>
2016-11-21 17:51:09 -0600

    Fix --makeconf flags on initial boot

--- run.sh
+++ run.sh
@@ -3,10 +3,12 @@
 # Generate the default configs if there isn't one specified.
 mkdir -p /home/znc
 chown -R znc:znc /home/znc
+
+flags=''
 if [[ ! -f /home/znc/.znc ]];
 then
-     su - znc -s /bin/sh -c 'znc --makeconf'
+    flags='--makeconf'
 fi
 
 # Start znc
-exec su - znc -s /bin/sh -c 'znc --foreground'
+exec su - znc -s /bin/sh -c "znc $flags --foreground"
